d8cebce74d8285822bcc479f7ff0929386ee8320
ian <ianbitok6465@gmail.com>
2024-08-19 23:38:15 +0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-auth")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
